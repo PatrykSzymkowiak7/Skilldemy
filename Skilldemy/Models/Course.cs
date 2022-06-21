6199007bfd2cc44dde1df7717244d3c81bd6e95a
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2022-06-21 17:48:22 +0200

    Course model + GlobalConfiguration packages

---
+++ Skilldemy/Models/Course.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Skilldemy.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter course's title")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Please enter course's description")]
+        public string Description { get; set; }
+
+        public int OwnerId { get; set; }
+
+        public List<Tag> Tags { get; set; }
+    }
+}
---
+++ Skilldemy/Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Skilldemy.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter tag name")]
+        public string Name { get; set; }
+    }
+}
