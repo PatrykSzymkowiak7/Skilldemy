9701091aa518da8a6bf27b4c329f3b5be2af9850
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2022-06-21 17:31:03 +0200

    Change of project v2

---
+++ Skilldemy/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Skilldemy.Startup))]
+namespace Skilldemy
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
