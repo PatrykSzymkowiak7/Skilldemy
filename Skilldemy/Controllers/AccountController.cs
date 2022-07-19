3a042f9c098c590b3913d9b3cbf6ec83f214df10
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2022-07-19 17:36:23 +0200

    Rejestracja - utworzenie folderu
    Po pomyślnej rejestracji dla użytkownika będzie tworzył się nowy folder na jego pliki w /UserFiles/{NazwaUżytkownika}

--- Skilldemy/Controllers/AccountController.cs
+++ Skilldemy/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -86,7 +87,7 @@ namespace Skilldemy.Controllers
                     return RedirectToAction("SendCode", new { ReturnUrl = returnUrl, RememberMe = model.RememberMe });
                 case SignInStatus.Failure:
                 default:
-                    ModelState.AddModelError("", "Invalid login attempt.");
+                    ModelState.AddModelError("", "Wprowadzony adres email lub hasło są błędne.");
                     return View(model);
             }
         }
@@ -156,12 +157,16 @@ namespace Skilldemy.Controllers
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
-                    
+
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
                     // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+
+                    var path = Server.MapPath(String.Format("~/UserFiles/{0}", user.UserName));
+                    Directory.CreateDirectory(path);
 
                     return RedirectToAction("Index", "Home");
                 }
---
+++ Skilldemy/Migrations/202207181814295_AnotherUserModelChanges.cs
@@ -0,0 +1,18 @@
+﻿namespace Skilldemy.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AnotherUserModelChanges : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Town", c => c.String(unicode: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Town");
+        }
+    }
+}
