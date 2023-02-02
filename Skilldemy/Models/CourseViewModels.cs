3a4e3702fcb10f5eff7afde38e365eae10775395
PatrykSzymkowiak7 <87982924+PatrykSzymkowiak7@users.noreply.github.com>
2023-02-02 18:35:43 +0100

    Tłumaczenia walidacji i przekroczone wejścia

--- Skilldemy/Models/CourseViewModels.cs
+++ Skilldemy/Models/CourseViewModels.cs
@@ -12,20 +12,20 @@ namespace Skilldemy.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Tytuł")]
         [Display(Name = "Tytuł")]
         public string Title { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Opis")]
         [Display(Name = "Opis")]
         public string Description { get; set; }
 
         public string OwnerId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Cena")]
         [Display(Name = "Cena")]
-        [Range(1, 2000)]
-        [DataType(DataType.Currency)]
+        [Range(1, 2000, ErrorMessage = "Cena musi być z zakresu od 1 - 2000 zł")]
+        [DataType(DataType.Currency, ErrorMessage = "Pole musi być typu walutowego")]
         [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
 
@@ -33,15 +33,15 @@ namespace Skilldemy.Models
 
         public DateTime CreatedDate { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Miniaturka")]
         [Display(Name = "Miniaturka")]
         public HttpPostedFileBase ImageFile { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Wideo prezentujące kurs")]
         [Display(Name = "Wideo prezentujące kurs")]
         public HttpPostedFileBase PreviewVideoFile { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Wideo Płatne")]
         [Display(Name = "Wideo Płatne")]
         public HttpPostedFileBase PaidVideoVile { get; set; }
 
@@ -107,7 +107,7 @@ namespace Skilldemy.Models
 
         public string OwnerId { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Pole wymagane - Cena")]
         [Display(Name = "Cena")]
         [DataType(DataType.Currency)]
         [Column(TypeName = "decimal(18, 2)")]
